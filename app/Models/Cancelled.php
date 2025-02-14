0eefb98cb1894f570e91fc40eae8444f95a453f9
OmesCT <premiumomez@gmail.com>
2025-02-14 19:44:04 +0700

    Add Databases and link Table_statuses and Users

---
+++ app/Models/Cancelled.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cancelled extends Model
+{
+    protected $table = 'cancelled';
+    protected $fillable = ['table_id', 'employee_id', 'customer_id', 'cancelled_at'];
+
+    public function table()
+    {
+        return $this->belongsTo(Tables::class, 'table_id');
+    }
+
+    public function employee()
+    {
+        return $this->belongsTo(Employees::class, 'employee_id');
+    }
+
+    public function customer()
+    {
+        return $this->belongsTo(Customers::class, 'customer_id');
+    }
+}
